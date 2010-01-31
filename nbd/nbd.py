48ffdb37c5335e2925dd6dcf6ace8b45b7328bc6
Vincent Pelletier <plr.vincent@gmail.com>
2010-01-31 11:17:22 +0100

    Document NBDServer class and remove debugging code.

--- nbd/nbd.py
+++ nbd/nbd.py
@@ -30,11 +30,37 @@ def answer(sock, handle, error=0, data=''):
     sock.sendall(data)
 
 class NBDServer(object):
+  """
+    Python implementation of MBD protocol.
+
+    Socket handling & event loop must be done outside of this class.
+  """
+
   def __init__(self, device):
+    """
+      device
+        Instance implementing the following methods:
+          getSize() -> int
+            Size of the underlying storage.
+          write(offset, data)
+            Write <data> starting at <offset>.
+          read(offset, length) -> string
+            Read <length> bytes starting at <offset>.
+    """
     self._device = device
     self._size = device.getSize()
 
   def greet(self, sock, read_only=False):
+    """
+      To be called upon socket connection establishment.
+      Sends NBD server greeting sequence, device size, and whether it is
+      read-only.
+
+      read_only (bool)
+        Whether the device should be advertised as allowing writes.
+        This is enforced withing this class, so that a client ignoring this
+        information will be refused to write anyway.
+    """
     sock.sendall(NBD_GREETING_PREFIX)
     flags = NBD_FLAG_HAS_FLAGS
     if read_only:
@@ -45,6 +71,10 @@ class NBDServer(object):
 
   def handle(self, sock):
     """
+      To be called upon incomming data on socket.
+      Blocks until an entire command has been received, and response had been
+      sent back.
+
       Return values:
         True: operation can continue on socket
         False: socket must be closed
@@ -56,11 +86,10 @@ class NBDServer(object):
       return False
     request = sock.recv(NBD_REQUEST_LEN)
     if len(request) != NBD_REQUEST_LEN:
-      import pdb; pdb.set_trace()
+      return False
     (action, handle, offset, length) = unpack(
       NBD_REQUEST_FORMAT, request)
     if action == NBD_ACTION_WRITE:
-      print 'NBD: write 0x%x bytes at 0x%x' % (length, offset)
       data = sock.recv(length)
       if len(data) != length:
         result = False
@@ -74,19 +103,16 @@ class NBDServer(object):
           raise
         answer(sock, handle)
     elif action == NBD_ACTION_READ:
-      print 'NBD: read 0x%x bytes at 0x%x' % (length, offset)
       try:
         data = self._device.read(offset, length)
       except:
         answer(sock, handle, error=ERROR_OTHER)
         raise
       if len(data) != length:
-        import pdb; pdb.set_trace()
         answer(sock, handle, error=ERROR_SHORT_READ)
         result = False
       answer(sock, handle, data=data)
     elif action == NBD_ACTION_DISCONNECT:
-      print 'NBD: disconnect'
       result = False
     else:
       answer(sock, handle, error=ERROR_NOT_IMPLEMENTED)
