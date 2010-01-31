e1095c7f8773dbdd3e8529a0d633a96f19162011
Vincent Pelletier <plr.vincent@gmail.com>
2010-01-31 22:11:00 +0100

    Remove functions to read save title and icon.
    Those functions are not to be handled at filesystem level.

--- fuse/ps1.py
+++ fuse/ps1.py
@@ -9,22 +9,9 @@ PRODUCT_CODE_OFFSET = 0xc
 PRODUCT_CODE_LENGTH = 0xa
 GAME_CODE_OFFSET = 0x16
 
-SAVE_TITLE_OFFSET = 0x4
-SAVE_TITLE_LENGTH = 0x5c # XXX: is it true ?
-PALETTE_OFFSET = 0x60
-PALETTE_ENTRY_FORMAT = '>h'
-PALETTE_ENTRY_LEN = calcsize(PALETTE_ENTRY_FORMAT)
-PALETTE_ENTRY_COUNT = 0xf
-IMAGE_COUNT_OFFSET = 0x2
-IMAGE_COUNT_MASK = 0x3
-IMAGE_OFFSET = 0x80
-IMAGE_LENGTH = 0x80
-
-PSX_DIRECTORY_FREE = 0xA0
 PSX_DIRECTORY_USED = 0x50
 PSX_BLOCK_TOP = 0x01
 PSX_BLOCK_LINK = 0x02
-PSX_BLOCK_LINK_END = 0x03
 
 class PS1Card(object):
   def __init__(self, device):
@@ -66,8 +53,6 @@ class PS1Card(object):
         yield PS1Save(self, block_number)
 
 class PS1Save(object):
-  _title = None
-  _icon = None
 
   def __init__(self, card, first_block_number):
     self._card = card
@@ -91,43 +76,4 @@ class PS1Save(object):
   def getId(self):
     return str(self._block_list[0])
 
-  def getTitle(self):
-    title = self._title
-    if title is None:
-      encoded_title = self._card.readBlock(self._block_list[0])[ \
-        SAVE_TITLE_OFFSET:SAVE_TITLE_OFFSET + SAVE_TITLE_LENGTH]
-      char_list = []
-      append = char_list.append
-      for char_index in xrange(0, SAVE_TITLE_LENGTH, 2):
-        encoded_char = encoded_title[char_index:char_index + 2]
-        if encoded_char[0] == '\x00':
-          break
-        append(encoded_char)
-      title = ''.join(char_list).decode('shift_jis')
-      self._title = title
-    return title
-
-  def getIconList(self):
-    icon = self._icon
-    if icon is None:
-      block = self._card.readBlock(self._block_list[0])
-      palette = []
-      append = palette.append
-      for x in xrange(PALETTE_ENTRY_COUNT):
-        palette_bgr_value = unpack(PALETTE_ENTRY_FORMAT,
-          block[PALETTE_OFFSET + x * PALETTE_ENTRY_LENGTH])[0]
-        r_value = (palette_bgr_value & 0x1f) * 8
-        g_value = (palette_bgr_value >> 5 & 0x1f) * 8
-        b_value = (palette_bgr_value >> 10 & 0x1f) * 8
-        append((r_value << 16) | (g_value << 8) | b_value)
-      icon = []
-      for icon_id in xrange(ord(block[IMAGE_COUNT_OFFSET]) & IMAGE_COUNT_MASK):
-        icon_frame = []
-        append = icon_frame.append
-        icon_offset = IMAGE_OFFSET + IMAGE_LENGTH * icon_id
-        for pixel_pair in block[icon_offset:icon_offset + IMAGE_LENGTH]:
-          append(palette[pixel_pair >> 4])
-          append(palette[pixel_pair & 0xf])
-        icon.append(icon_frame)
-    return icon
 
